04d4a12dfc69a6a1e8246d8c3b2b7516a364ec52
nhatlq <luongquocnhat@gmail.com>
2019-11-08 17:28:19 +0700

    master

---
+++ resources/views/admin_master/header.blade.php
@@ -0,0 +1,67 @@
+<header class="main-header container">
+    <div class="" style="padding-top: 20px">
+        <nav class="nav_signin">
+            <ul>
+                <li class="btn_open_menu lg_hide">
+                    <a>
+                        <i style="height: 2px; width: 25px; background: darkslategrey; display: block;margin-bottom: 5px;"></i>
+                        <i style="height: 2px; width: 25px; background: darkslategrey; display: block;margin-bottom: 5px;"></i>
+                        <i style="height: 2px; width: 25px; background: darkslategrey; display: block;margin-bottom: 5px;"></i>
+                    </a>
+                </li>
+                <li class="lg_hide learn">
+                    <a href="#">Đăng ký học thử </a>
+                </li>
+                <li class="signin">
+                    <a>Sign in</a>
+                </li>
+                <li class="language">
+                    <a href="#">VN</a>
+                    |
+                    <a href="#">EN</a>
+                </li>
+            </ul>
+        </nav>
+        <nav class="nav_menu">
+            <ul>
+                <li>
+                    <a href="#">Trang chu</a>
+                </li>
+                <li>
+                    <a href="#">Về Educoo</a>
+                </li>
+                <li class="exist_sub">
+                    <a class=""  href="#">Các Khoá Học</a>
+                    <ul class="sub_menu">
+                        <li>
+                            <a href="#">Tiếng anh cho trẻ</a>
+                        </li>
+                        <li>
+                            <a href="#">Tiếng anh giao tiếp</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="exist_sub">
+                    <a href="#" class="dropdown-toggle">Học liệu Tiếng Anh</a>
+                    <ul class="sub_menu">
+                        <li>
+                            <a>Trình độ của bạn</a>
+                        </li>
+                        <li>
+                            <a>Trình độ cơ bản</a>
+                        </li>
+                    </ul>
+                </li>
+                <li >
+                    <a href="#">Tin tức</a>
+                </li>
+                <li >
+                    <a href="#">Tuyển dụng</a>
+                </li>
+                <li>
+                    <a href="#">Đăng ký học thử</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
